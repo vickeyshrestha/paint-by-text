da8bf400be4ec1dea6f2a368fd0becc84d6a4ef0
vickeyshrestha <logontovickey@hotmail.com>
2024-03-12 16:07:34 -0500

    - added a msg that rendering takes time

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import { getRandomSeed } from "lib/seeds";
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
-export const appName = "Paint by Text";
+export const appName = "VS-TextToPaint";
 export const appSubtitle = "Edit your photos using written instructions, with the help of an AI.";
 export const appMetaDescription = "Edit your photos using written instructions, with the help of an AI.";
 
